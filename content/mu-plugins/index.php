71f001c374932fac0f01e6fe10bcfd5e5c1453d8
Micah Wood <micah@wpscholar.com>
2020-06-11 15:07:15 -0400

    Default to disabling ssl verification on my local

--- content/mu-plugins/index.php
+++ content/mu-plugins/index.php
@@ -3,3 +3,5 @@
 ini_set( 'xdebug.var_display_max_children', - 1 ); // phpcs:ignore
 ini_set( 'xdebug.var_display_max_data', - 1 ); // phpcs:ignore
 ini_set( 'xdebug.var_display_max_depth', - 1 ); // phpcs:ignore
+
+add_filter( 'https_ssl_verify', '__return_false' );

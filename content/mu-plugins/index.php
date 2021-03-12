3670215b4910cef2b2bc9ccc486d47316abddb89
Micah Wood <micah@wpscholar.com>
2021-03-12 15:10:36 -0500

    Placeholder for remote media loader

--- content/mu-plugins/index.php
+++ content/mu-plugins/index.php
@@ -5,3 +5,16 @@ ini_set( 'xdebug.var_display_max_data', - 1 ); // phpcs:ignore
 ini_set( 'xdebug.var_display_max_depth', - 1 ); // phpcs:ignore
 
 add_filter( 'https_ssl_verify', '__return_false' );
+
+
+/**
+ * Loads media from a remote site when on a local dev environment.
+ * Eliminates the need to download the uploads directory from the remote site for testing purposes.
+ */
+//if ( 'example.local' === $_SERVER['HTTP_HOST'] ):
+//	add_filter( 'upload_dir', function ( $uploads ) {
+//		$uploads['baseurl'] = str_replace('example.local', 'example.com', $uploads['baseurl']);
+//
+//		return $uploads;
+//	} );
+//endif;
